98fca83f49c06493ddf2d005d8c118fe88807376
quezmes <quezmes@gmail.com>
2021-05-22 12:50:37 +0200

    Dodaj pliki projektów.

---
+++ DatesRanger/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DatesRanger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
