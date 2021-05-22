0726baa8e27b94a62ba15ccbdc228fefdee22fbc
quezmes <quezmes@gmail.com>
2021-05-22 13:22:22 +0200

    Add date range service instance to program class and onvoke get range method

--- DatesRanger/Program.cs
+++ DatesRanger/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DatesRanger.Services;
+using System;
 using System.Linq;
 
 namespace DatesRanger
@@ -7,26 +8,30 @@ namespace DatesRanger
     {
         private static readonly string _incorrectFirstDateErrorText = "First date is incorrect.";
         private static readonly string _incorrectSecondDateErrorText = "Second date is incorrect.";
+        private static DatesRangeService _dateRangeService = new DatesRangeService();
 
         static void Main(string[] args)
         {
             bool isDatesCorrect = true;
             DateTime dt1, dt2;
 
-            if(args.Count() == 2)
+            if (args.Count() == 2)
             {
                 if (DateTime.TryParse(args[0], out dt1) == false)
                 {
                     Console.WriteLine(_incorrectFirstDateErrorText);
                     isDatesCorrect = false;
                 }
-                if(DateTime.TryParse(args[1], out dt2) == false)
+                if (DateTime.TryParse(args[1], out dt2) == false)
                 {
                     Console.WriteLine(_incorrectSecondDateErrorText);
                 }
                 if (isDatesCorrect)
                 {
-                    
+                    if (dt1 < dt2)
+                        Console.WriteLine(_dateRangeService.GetDatesRange(dt1, dt2));
+                    else if (dt1 > dt2)
+                        Console.WriteLine(_dateRangeService.GetDatesRange(dt2, dt1));
                 }
 
 
