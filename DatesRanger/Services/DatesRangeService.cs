02bb1f07515faa515bd787cf02def94eedede15d
quezmes <quezmes@gmail.com>
2021-05-22 14:01:12 +0200

    Delete useless is statment in getDatesRange method

--- DatesRanger/Services/DatesRangeService.cs
+++ DatesRanger/Services/DatesRangeService.cs
@@ -9,14 +9,11 @@ namespace DatesRanger.Services
 {
     public class DatesRangeService
     {
-        private readonly string _theSameDatesErrorText = "You provided two same dates.";
+
         public string GetDatesRange(DateTime date1, DateTime date2)
         {
             if (date1.Year == date2.Year)
                 if (date1.Month == date2.Month)
-                    if (date1.Day == date2.Day)
-                        return _theSameDatesErrorText;
-                    else
                         return GetDatesRangeTextResult(date1.ToString("dd"), date2.ToString("dd"));
                 else
                     return GetDatesRangeTextResult(date1.ToDayAndMonthString(), date2.ToDayAndMonthString());
