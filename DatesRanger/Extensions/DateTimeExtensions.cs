416b9c91ddf4f9517e0ff2872e4eb061bf52c88e
quezmes <quezmes@gmail.com>
2021-05-22 13:53:05 +0200

    Add fuction to check if month is after day or reverse

--- DatesRanger/Extensions/DateTimeExtensions.cs
+++ DatesRanger/Extensions/DateTimeExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +10,36 @@ namespace DatesRanger.Extensions
 {
     public static class DateTimeExtensions
     {
+        enum MonthLocationPattern
+        {
+            BeforeDay,
+            AfterDay
+        }
+
+        static readonly char _dayChar = 'd';
+        static readonly char _monthChar = 'M';
+
         public static string ToDayAndMonthString(this DateTime date)
         {
-            return date.ToString();
+            return date.ToString(GetMonthDataPattern());
+        }
+
+        private static string GetMonthDataPattern()
+        {
+            var shortDataPattern = DateTimeFormatInfo.CurrentInfo.ShortDatePattern;
+            char firstChar, secondChar;
+            if (GetMonthLocationPatter() == MonthLocationPattern.AfterDay)
+                return "";
+            return "";
+        }
+
+        private static MonthLocationPattern GetMonthLocationPatter()
+        {
+            var shortDatePattern = DateTimeFormatInfo.CurrentInfo.ShortDatePattern;
+            if (shortDatePattern.IndexOf(_dayChar) < shortDatePattern.IndexOf(_monthChar))
+                return MonthLocationPattern.AfterDay;
+            else
+                return MonthLocationPattern.BeforeDay;
         }
     }
 }
