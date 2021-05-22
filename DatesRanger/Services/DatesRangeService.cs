8385d7b6b43f3cdb2d7f8eafe28b6c2f5cc2ac3f
quezmes <quezmes@gmail.com>
2021-05-22 13:04:13 +0200

    Create dates range service

---
+++ DatesRanger/Services/DatesRangeService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatesRanger.Services
+{
+    public class DatesRangeService
+    {
+        private readonly string _theSameDatesErrorText = "You provided two same dates.";
+        public string GetDatesRange(DateTime date1, DateTime date2)
+        {
+            if (date1.Year == date2.Year)
+                if (date1.Month == date2.Month)
+                    if (date1.Day == date2.Day)
+                        return _theSameDatesErrorText;
+        }
+    }
+}
